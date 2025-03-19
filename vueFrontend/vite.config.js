1f78568af37fc276be6802804657d2d20b37cb12
notbraison <orinabraison@gmail.com>
2025-03-19 17:21:08 +0300

    github actions changes for vue

--- vueFrontend/vite.config.js
+++ vueFrontend/vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  publicPath: '/Rentika/'  //you have to add your repo name here 
+})
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
