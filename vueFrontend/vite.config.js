4e3e62e4e7cf03309f9032a023f9b2543a46a513
notbraison <orinabraison@gmail.com>
2025-03-19 20:43:44 +0300

    s

--- vueFrontend/vite.config.js
+++ vueFrontend/vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  publicPath: '/Rentika/'  //you have to add your repo name here 
-})
-
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/Rentika',
   plugins: [vue()],
   server:{
     port:3500
