3b6036e1303317772b381b5207af5c5b29bee478
notbraison <orinabraison@gmail.com>
2025-03-19 21:07:23 +0300

    Prepare version-one branch for GitHub Pages

--- vueFrontend/vite.config.js
+++ vueFrontend/vite.config.js
@@ -3,8 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/Rentika',
   plugins: [vue()],
+  base:'/Rentika',
   server:{
     port:3500
   }
