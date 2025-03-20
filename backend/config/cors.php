0b28a090ea5d3d80273c95eef98866be31a0ebd0
notbraison <orinabraison@gmail.com>
2025-03-20 21:02:14 +0300

    development branch created , main is now production branch

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -2,10 +2,10 @@
 
 return [
 'paths' => ['api/*', 'sanctum/csrf-cookie','auth/*'],
-'allowed_methods' => ['*'],
+'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
 'allowed_origins' => ['http://localhost:3500','http://127.0.0.1:3500'],
 'allowed_origins_patterns' => [],
-'allowed_headers' => ['*'],
+'allowed_headers' => ['Content-Type', 'X-Auth-Token', 'Origin', 'Authorization'],
 'exposed_headers' => [],
 'max_age' => 0,
 'supports_credentials' => true,
