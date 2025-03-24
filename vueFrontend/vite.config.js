8fa6800ef485741a24fefee17e27498c0f7685bb
notbraison <orinabraison@gmail.com>
2025-03-24 14:13:01 +0300

    some changes to better deploy

--- vueFrontend/vite.config.js
+++ vueFrontend/vite.config.js
@@ -3,6 +3,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
   base:'/Rentika',
   plugins: [vue()],
   server:{
