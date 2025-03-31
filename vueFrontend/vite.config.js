4ae1a8a47c7432e965559af7c29b2bca42d584d4
notbraison <orinabraison@gmail.com>
2025-03-31 11:22:33 +0300

    backend docker added for deploy render

--- vueFrontend/vite.config.js
+++ vueFrontend/vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Rentika/',  // Ensure this matches your project name on Render
+  plugins: [vue()],
+  server:{
+    port:3500
+  },
   build: {
     rollupOptions: {
       output: {
@@ -13,10 +17,6 @@ export default defineConfig({
         }
       }
     }
-  },
-  base:'/',
-  plugins: [vue()],
-  server:{
-    port:3500
   }
-})
+});
+
